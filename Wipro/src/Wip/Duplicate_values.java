ea9cb089290034c768e74b073a8960595b9b26f6
Arul <Arul@Arul-PC>
2019-02-11 20:10:24 +0530

    Meera_1

---
+++ Wipro/src/Wip/Duplicate_values.java
@@ -0,0 +1,57 @@
+package Wip;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+
+
+public class Duplicate_values  {
+
+@Test
+	public static void ma() 
+	{
+	
+String[] Dupliacte={"Arul","Nithya","Punitha","Arul","Nithya"};
+
+for(int i=0;i<Dupliacte.length;i++)
+{
+	
+	for(int j=i+1;j<Dupliacte.length;j++)
+	{
+		
+		if(Dupliacte[i].equals(Dupliacte[j]))
+		{
+			System.out.println("Dupliacte:----- "+ Dupliacte[i] );
+		}
+	}
+}
+	
+	Set<String> Store=new HashSet<String>();
+	
+	for(String Dup:Dupliacte)
+	{
+		if(Store.add(Dup)==false)
+		{
+			System.out.println("Dupliacte:----- "+ Dup);	
+		}
+	}
+	
+	int [] Num={5,6,2,4,8,5};
+	Set<Integer> stor=new HashSet<Integer>();
+	for(int Nu:Num)
+	{
+		if(stor.add(Nu)==false)
+		{
+			System.out.println("Dupliacte:----- "+ Nu);	
+		}
+	}
+	
+}
+	
+	}
+
+
+	
+	
